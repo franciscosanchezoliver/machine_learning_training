b21a39f06cb980bb80d9d414356bb994619e85e0
Francisco Sanchez Oliver <spuzi@hotmail.es>
2023-11-08 07:49:43 +0100

    teoria de llm

---
+++ regression/07_building_a_logistic_regression_model/07_building_a_logistic_regression_model.py
@@ -0,0 +1,18 @@
+"""
+Building a logistic regression model
+------------------------------------
+In this exercise, you will build a logistic regression model using all features in the
+diabetes_df dataset. The model will be used to predict the probability of individuals
+in the test set having a diabetes diagnosis.
+
+We'll use the diabetes dataset.
+
+To do:
+- Split the data into train and test set.
+- Create a Logistic Regression model.
+- Fit it to the train set.
+- Predict the probabilities of the test.
+
+"""
+
+import pandas as pd
